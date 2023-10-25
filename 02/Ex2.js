ac61f02a5829fe44e5de7650b820022a4f910638
JerryHwung <44294098+JerryHwung@users.noreply.github.com>
2023-10-26 00:45:53 +0800

    Day 2

---
+++ 02/Ex2.js
@@ -0,0 +1,28 @@
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+console.log('\"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead.\"')
+//Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+let ten='10'
+let tenInt = parseInt(ten)
+console.log(typeof(tenInt)==typeof 10)
+//Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+let num='9.8'
+let numFloat=parseFloat(num)
+console.log(Math.round(numFloat))
+//Check if 'on' is found in both python and jargon
+console.log('python'.includes('on'))
+console.log('jargon'.includes('on'))
+//I hope this course is not full of jargon. Check if jargon is in the sentence.
+let sentence='I hope this course is not full of jargon.'
+console.log(sentence.includes('jargon'))
+//Generate a random number between 0 and 100 inclusively.
+let rand=Math.floor(Math.random() * 101)
+console.log(rand)
+//Generate a random number between 50 and 100 inclusively.
+let rand50=Math.floor(Math.random() * 51)
+console.log(rand50+50)
+//Generate a random number between 0 and 255 inclusively.
+let rand255=Math.floor(Math.random()*256)
+console.log(rand255)
+//Access the 'JavaScript' string characters using a random number.
+//??
+
